cac630707736e5a61c2a24999b65284e06cfa826
tk-pato <tk.patolaqshe@gmail.com>
2025-11-17 18:52:54 +0900

    Update uservoice dummy data: change from cram school to bust-up salon testimonials

--- swell_child/template-parts/front/section-uservoice.php
+++ swell_child/template-parts/front/section-uservoice.php
@@ -64,11 +64,11 @@ if (!defined('ABSPATH')) exit;
                             <div class="feedback-image">
                                 <img src="<?php echo get_stylesheet_directory_uri() . '/img/nav/salon.png'; ?>" alt="お客様1" class="customer-img" />
                             </div>
-                            <h3 class="feedback-title">クラスマー楽になった</h3>
+                            <h3 class="feedback-title">自分に自信が持てるようになった</h3>
                             <div class="feedback-content">
-                                <p>テストの成績がアップしました。次のテストも高得点狙いたいです。</p>
+                                <p>施術を受けてから姿勢が良くなり、バストのラインが綺麗になりました。鏡を見るのが楽しみです。</p>
                             </div>
-                            <div class="feedback-author">小学3年生</div>
+                            <div class="feedback-author">30代女性</div>
                             <div class="feedback-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -83,11 +83,11 @@ if (!defined('ABSPATH')) exit;
                             <div class="feedback-image">
                                 <img src="<?php echo get_stylesheet_directory_uri() . '/img/nav/salon.png'; ?>" alt="お客様2" class="customer-img" />
                             </div>
-                            <h3 class="feedback-title">苦手だった数学が得意になった！</h3>
+                            <h3 class="feedback-title">諦めていたけど変化を実感</h3>
                             <div class="feedback-content">
-                                <p>SWELL授業は面白いですよ。すごく苦手だった数学が得意になった。テストの点数も上がったです。</p>
+                                <p>年齢的に無理だと思っていましたが、3ヶ月でサイズアップ！スタッフの方々も親切で通いやすいです。</p>
                             </div>
-                            <div class="feedback-author">小学5年生</div>
+                            <div class="feedback-author">40代女性</div>
                             <div class="feedback-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
@@ -102,11 +102,11 @@ if (!defined('ABSPATH')) exit;
                             <div class="feedback-image">
                                 <img src="<?php echo get_stylesheet_directory_uri() . '/img/nav/salon.png'; ?>" alt="お客様3" class="customer-img" />
                             </div>
-                            <h3 class="feedback-title">全国順位で一位になった</h3>
+                            <h3 class="feedback-title">体質改善にも効果あり</h3>
                             <div class="feedback-content">
-                                <p>日記にはいつものことですが、全国順位で一位になった。SWELLは素晴らしいおかげです。</p>
+                                <p>バストケアだけでなく、冷え性も改善されて驚きです。身体全体が軽くなった感じがします。</p>
                             </div>
-                            <div class="feedback-author">小学5年生</div>
+                            <div class="feedback-author">20代女性</div>
                             <div class="feedback-rating">
                                 <i class="fa fa-star"></i>
                                 <i class="fa fa-star"></i>
